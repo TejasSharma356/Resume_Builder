7705587d4a6436f49e4f30fb9b72718f78a6a316
TejasSharma356 <you@example.com>
2025-08-29 00:51:06 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 
 export default defineConfig({
   plugins: [react()],
   base: "/Resume_uilder/",  // 👈 repo name here
-=======
-import path from 'path'
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
->>>>>>> d876bbe4e477a9b14685c19819f090b923c54511
 })
