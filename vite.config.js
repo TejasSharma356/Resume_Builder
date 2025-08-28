43df40420f3aa202687870c845c9c8f9dd725c85
TejasSharma356 <you@example.com>
2025-08-29 00:39:52 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Resume-Builder/",  // 👈 repo name here
+  base: "/Resume_uilder/",  // 👈 repo name here
 })
